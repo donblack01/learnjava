98808078bcf84d0cf7074a574b7df9f8a69df305
ty <ty@tu.com>
2019-07-25 17:23:37 +0800

    'java基础'

--- HelloWorld/src/tempCodeRunnerFile.java
+++
@@ -1,9 +0,0 @@
-for (int i = 1; i <= a; i++) {
-  for (int k = 1; k <= a-i;k++){
-    System.out.print(" ");
-  }
-  for (int j = 1; j <= 2*i -1 ; j++) {
-    System.out.print("*");
-  }
-  System.out.println();
-}
